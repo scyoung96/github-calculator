4bc0af91c2b40332631be9ccbc4b6fff88e61442
Spencer Young <scyoung96@gmail.com>
2024-02-16 18:30:18 -0700

    implement Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing Dockerfile");
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        while (true) {
+            String command = scanner.next();
+
+            if (command.equals("quit") || command.equals("exit")) {
+                break;
+            }
+            
+            if (command.equals("add")) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                int result = calculator.add(a, b);
+                System.out.println(result);
+            } else if (command.equals("subtract")) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                int result = calculator.subtract(a, b);
+                System.out.println(result);
+            } else if (command.equals("multiply")) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                int result = calculator.multiply(a, b);
+                System.out.println(result);
+            } else if (command.equals("divide")) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                int result = calculator.divide(a, b);
+                System.out.println(result);
+            } else if (command.equals("fibonacciNumberFinder")) {
+                int n = scanner.nextInt();
+                int result = calculator.fibonacciNumberFinder(n);
+                System.out.println(result);
+            } else if (command.equals("intToBinaryNumber")) {
+                int number = scanner.nextInt();
+                String result = calculator.intToBinaryNumber(number);
+                System.out.println(result);
+            }
+        }
+        
+        scanner.close();
     }
 }
