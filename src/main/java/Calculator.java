7f30a617b949bac68ec596c1d5f6779ce21f3cb3
Spencer Young <scyoung96@gmail.com>
2024-02-16 17:48:21 -0700

    test attempt: 1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -41,17 +41,17 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        int fibNum = 0;
-        int fibNumPrev = 1;
-        int fibNumPrevPrev = 0;
-        
-        for (int i = 1; i <= n; i++) {
-            fibNum = fibNumPrev + fibNumPrevPrev;
-            fibNumPrevPrev = fibNumPrev;
-            fibNumPrev = fibNum;
+        if (n <= 1) {
+            return n;
         }
-        
-        return fibNum;
+        int fib = 1;
+        int prevFib = 1;
+        for (int i = 2; i < n; i++) {
+            int temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        }
+        return fib;
     }
             
 
